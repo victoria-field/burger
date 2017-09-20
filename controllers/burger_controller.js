6472375c4dfda64da8ea65922c83330f161212d1
victoria field <victoria.shadid@gmail.com>
2017-09-20 09:48:38 -0500

    connected files. started handlebars

---
+++ controllers/burger_controller.js
@@ -0,0 +1,49 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var tasty = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  tasty.all(function(data) {
+    var hbsObject = {
+      yum: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  tasty.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.name, req.body.sleepy
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  tasty.update({
+    devoured: req.body.sleepy
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+router.delete("/:id", function(req, res){
+//  we want to delete this burger
+
+  tasty.delete("id =" + req.params.id, function(res){
+    res.redirect("/");
+  })
+});
+// Export routes for server.js to use.
+module.exports = router;
