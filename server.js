d90fb0c4fb83ec3a03bcd2e108489d3173d336be
victoria field <victoria.shadid@gmail.com>
2017-09-21 12:54:23 -0500

    added css.css

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var path = require('path')
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use('/', express.static(path.resolve(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
